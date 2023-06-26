3834b682848a074fbe3e0f2c2f509608464276df
Manish kumar <kmanishraj5@gmail.com>
2023-06-26 21:52:10 +0530

    Longest Common SubSequence

---
+++ LongComSubseq.java
@@ -0,0 +1,26 @@
+class Solution
+{
+
+    static int lcs(int m, int n, String s1, String s2)
+    {
+        
+        int dp[][] = new int[m+1][n+1];
+        for(int i=0;i<=m;i++)
+            dp[i][0] = 0;
+        for(int i=0;i<=n;i++)
+            dp[0][i] = 0;
+        for(int i=1;i<=m;i++)
+        {
+            for(int j=1;j<=n;j++)
+            {
+                if(s1.charAt(i-1)==s2.charAt(j-1))
+                    dp[i][j] = 1+dp[i-1][j-1];
+                else
+                    dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+        return dp[m][n];
+        
+    }
+    
+}
