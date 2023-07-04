1fca90a153ecf11efd13dd21033e2681aeb881d6
Manish kumar <kmanishraj5@gmail.com>
2023-07-04 10:47:08 +0530

    Largest square formed in a matrix

---
+++ LargestSquareForMatrix.java
@@ -0,0 +1,19 @@
+class Solution{
+    static int maxSquare(int n, int m, int mat[][]){
+     int dp[][]= new int[n][m];
+     int ans=0;
+     for(int i=0;i<n;i++){
+         for(int j=0;j<m;j++){
+             if(i== 0 || j== 0){
+                 dp[i][j]=mat[i][j];
+             }else{
+                 if(mat[i][j]==1){
+                     dp[i][j]=Math.min(dp[i-1][j],Math.min(dp[i][j-1],dp[i-1][j-1]))+1;
+                 }
+             }
+             ans=Math.max(ans,dp[i][j]);
+         }
+     }
+     return ans;
+    }
+}
